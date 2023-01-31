e3a90ff4c74c1d6be2da57cb8f8df39a45b6eb2f
张毅 <>
2023-01-31 16:43:28 +0800

    no message

--- Test.php
+++ Test.php
@@ -1,5 +1,22 @@
 <?php
 
+use utils\tree\UserBinTree;
+
 require_once __DIR__."/vendor/autoload.php";
 
 //测试
+$r = UserBinTree::getInstance()
+	->initTreeRootNode()
+	->appendTreeNode(1,0,0)
+	->appendTreeNode(2,1,1)
+	->appendTreeNode(3,1,2)
+	->getTreeData();
+
+$r = UserBinTree::getInstance()
+	->appendTreeNode(4,2,1)
+	->getTreeData();
+
+unserialize($r);
+var_dump($r);
+
+
